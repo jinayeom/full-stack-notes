7d36056c605a0540c93e34988ae60c322270e956
jinayeom <jinayeom@tudents.cs.ubc.ca>
2025-10-02 18:46:27 -0700

    Added analogy explaining Django framework

--- src/app/django/page.js
+++ src/app/django/page.js
@@ -55,7 +55,46 @@ export default function Home() {
         </p>
 
         {/* Django Analogy Walkthrough */}
-        
+        <p className="body">
+        Imaging you got an internship at Meta. Congratulations! It’s your dream internship and you’re super excited. 
+        However, it’s your first day and you’re someone who likes to visualize what the day is going to look like. 
+        So, you run the steps through your head. 
+        </p>
+        <ol className="ordered-list">
+          <li className="ordered-item">
+          As an intern, you follow the address to a huge corporate building. As you enter the office, you enter the 
+          lobby and are met with a friendly front desk person. Similar to this, when navigating to a certain URL, you 
+          navigate to a <span className="font-bold"> site (corporate building)</span> and end up on the
+          <span className="font-bold"> landing page (lobby)</span>. In the backend, you are met with the <span className="font-bold"> URL configuration </span> 
+          for the site <span className="font-bold">(front desk person)</span>. 
+          </li>
+          <li className="ordered-item">
+          You specify to the front desk person that you are looking to get to the Meta floor. The front desk person informs you 
+          that Meta will be on the 8th floor, however, from there another front desk person will help you navigate on the floor. 
+          Similar to this, when you specify a specific URL endpoint, the <span className="font-bold"> site-level URL configuration (front desk) </span>
+          will direct you to the specific app you’re looking for <span className="font-bold"> (Meta's floor)</span>, but will then pass on the responsibility 
+          of helping you as the client to the <span className="font-bold"> app-level URL configuration (Meta's front desk)</span>. This is done via the 
+          <code className="inline-code">include()</code> function, which tells Django to look inside this URL configuration to look for the rest of the URL endpoints.
+          </li>
+          <Image 
+          src="/images/django/include-function.png"
+          alt="Django Include Function"
+          width={500}
+          height={500}
+          className="fade-
+          in-up rounded-lg"
+         />
+         <li className="ordered-item">
+          Once you make it to the Meta floor, you enter Meta’s lobby. You are met with another friendly front desk person for Meta, who asks where you need to go so they 
+          can help direct you. Similar to this, when you navigate to the <span className="font-bold"> app (Meta’s floor)</span>, you land on the <span className="font-bold"> index page</span>
+          for this application <span className="font-bold"> (Meta’s lobby)</span>, where you meet an <span className="font-bold"> app-level URL configuration (Meta’s front desk person) </span>
+          who is now responsible for helping you navigate the application. 
+         </li>
+        </ol>
+        <p className="body"> 
+          Now that you have a general understanding of the Django framework and the relationship between <span className="font-bold"> Projects vs Apps</span>,
+          it's now time to go through the Django tutorial on their official website. 
+        </p>
 
 
       
